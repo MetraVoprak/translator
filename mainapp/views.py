3c20b74450a29997b7cbffc975d39c894a2a876e
Artem <a2291067@gmail.com>
2023-02-07 00:05:50 +0300

    Step 2. Improve templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,37 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "mainapp/index.html")
+    title = 'Переводчик'
+    content = {"title": title}
+    return render(request, "mainapp/index.html", content)
 
 
 def all_words(request):
-    return render(request, "mainapp/all_words.html")
+    title = 'Все слова'
+    words = [
+        {
+            'eng':'hand', 
+            'rus':'рука'
+        },
+        {
+            'eng':'book', 
+            'rus':'книга'
+        },
+        {
+            'eng':'run', 
+            'rus':'бежать'
+        },
+        {
+            'eng':'city', 
+            'rus':'город'
+        }
+    ]
+    content = {"title": title, "words": words}
+    # content = {"title": title}
+    return render(request, "mainapp/all_words.html", content)
 
 
 def training(request):
-    return render(request, "mainapp/training.html")
+    title = 'Тренировка'
+    content = {"title": title}
+    return render(request, "mainapp/training.html", content) 
