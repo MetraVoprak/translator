5ee08024ae6634c35d989a1fc4cdad9a727a4b25
Artem <a2291067@gmail.com>
2023-03-27 16:24:33 +0300

    Step 4. Add user CRUD + new_css

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -1,13 +1,55 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
+
 from .models import Student
 
 
 class ShopUserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
-        super(ShopUserLoginForm, self). __init__(*args, **kwargs)
+        super(ShopUserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
 
     class Meta:
         model = Student
         fields = ("username", "password")
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(ShopUserRegisterForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+            field.help_text = ""
+
+    def clean_age(self):
+        data = self.cleaned_data["age"]
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+        return data
+
+    class Meta:
+        model = Student
+        # fields = ("username", "first_name", "password1",
+        #           "password2", "email", "age", "avatar")
+        fields = ("username", "first_name", "password1", "password2", "email", "age")
+
+
+class ShopUserEditForm(UserChangeForm):
+    def __init__(self, *args, **kwargs):
+        super(ShopUserEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+            field.help_text = ""
+
+    def clean_age(self):
+        data = self.cleaned_data["age"]
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+
+        return data
+
+    class Meta:
+        model = Student
+        # fields = ("username", "first_name", "email", "age", "avatar")
+        fields = ("username", "first_name", "email", "age")
